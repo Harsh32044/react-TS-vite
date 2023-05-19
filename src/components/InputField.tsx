b250b804d85274341c8e65c3647883e29628a288
Harsh Srivastav <hsrivastav45@gmail.com>
2023-05-19 19:24:20 +0530

    useRef to loose focus from input element on Enter Keypress

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import './styles.css'
 
 interface Props {
@@ -8,15 +8,18 @@ interface Props {
 }
 const InputField: React.FC<Props> = ({todo, setTodo, handleAdd}) => {
 
-  const inputVal = useRef<HTMLInputElement>(null)  
-
-  console.log(todo)
+  const inputRef = React.useRef<HTMLInputElement>(null)
   return (
     <form className="input" onSubmit={(event) => {
       handleAdd(event)
-      inputVal.current?.blur()
+      inputRef.current?.blur()
     }}>
-        <input ref={inputVal} type="input" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder="Enter a task" className="input__box" />
+        <input 
+          ref={inputRef} 
+          type="input" 
+          value={todo} 
+          onChange={(e) => setTodo(e.target.value)} 
+          placeholder="Enter a task" className="input__box" />
         <button className="input__submit" type="submit">GO</button>
     </form>
   )
