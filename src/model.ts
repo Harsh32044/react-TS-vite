155f318e4ce2a03fe4b99730b70179ccb69a5bd1
Harsh Srivastav <hsrivastav45@gmail.com>
2023-05-20 23:13:43 +0530

    Adding an Actions Type, for useReducer

--- src/model.ts
+++ src/model.ts
@@ -1,5 +1,12 @@
-export interface Todo {
+interface Todo {
     id: string,
     todo: string,
     isDone: boolean,
 }
+
+type Actions = {
+    type: 'add' | 'delete' | 'done',
+    payload: string
+  }
+
+export type { Todo, Actions }
